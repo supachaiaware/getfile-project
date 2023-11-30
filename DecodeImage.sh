3719e0b2a6825b0d2a75419721ea68d66bf6632c
Mr.Supachai Yossan <135792496+supachaiaware@users.noreply.github.com>
2023-11-30 13:54:33 +0700

    Update DecodeImage.sh

--- DecodeImage.sh
+++ DecodeImage.sh
@@ -7,13 +7,11 @@ GetIDcardImage() {
     grep ^2023.*.idCardImage* "${1}" | awk -F'|' '{print $1 "|" $9}' | sed 's/","/|/g; s/":"/|/g' | cut -d '|' -f1,7,49,57
 }
 
-
 DecodeBase64() {
 
   find "${1}" -type f -name "*.log" | while IFS= read -r i; do
 
         while IFS= read -r line; do
-
 
             TIMESTAMP=$(echo "$line" |cut -d '|' -f1 | tr -d ':')
             MOBILE_NUM=$(echo "$line" |cut -d '|' -f2)
