154801e0e6d78f65da5477e1423108e1381112af
Mr.Supachai Yossan <135792496+supachaiaware@users.noreply.github.com>
2023-12-20 13:39:35 +0700

    Update Replace_images.sh

--- Replace_images.sh
+++ Replace_images.sh
@@ -2,16 +2,18 @@
 
 ORIGINAL_FOLDER=${1}
 DECODE_PATH=${2}
-
+ 
 for i in "$ORIGINAL_FOLDER"/*.jpg; do
     if [ -f "$i" ] && [ ! -s "$i" ]; then
+    
         FILENAME=$(basename "$i")
-
         CUT_ORIGINAL=$(echo "$FILENAME" | awk -F '_' '{print $2"_"$3}')
-        MACTH_FILE=$(find "$DECODE_PATH" -type f -name "*${CUT_ORIGINAL}*")
+        MATCH_FILES=$(find "$DECODE_PATH" -type f -name "*${CUT_ORIGINAL}*")
+ 
+        if [ -n "$MATCH_FILES" ]; the
 
-        if [ -n "$MACTH_FILE" ]; then
-            cp "$MACTH_FILE" "$i"
+            FIRST_MATCH_FILE=$(echo "$MATCH_FILES" | head -n 1)
+            cp "$FIRST_MATCH_FILE" "$i"
             echo "$FILENAME: Replace completed"
         else
             echo "$FILENAME: No file in folder"
